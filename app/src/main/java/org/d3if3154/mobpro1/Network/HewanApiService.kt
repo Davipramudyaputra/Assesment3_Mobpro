083e94827c0d6a14332c28c8fceaaef08f7f20f2
Davipramudyaputra <daviputra551@gmail.com>
2024-06-12 18:14:20 +0700

    15-sending-data

--- app/src/main/java/org/d3if3154/mobpro1/Network/HewanApiService.kt
+++ app/src/main/java/org/d3if3154/mobpro1/Network/HewanApiService.kt
@@ -1,14 +1,21 @@
 package org.d3if3154.mobpro1.Network
 
+
+import org.d3if3154.mobpro1.model.Hewan
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
-import org.d3if3154.mobpro1.model.Hewan
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import org.d3if3154.mobpro1.model.OpStatus
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Multipart
+import retrofit2.http.POST
+import retrofit2.http.Part
 
-private const val BASE_URL = "https://raw.githubusercontent.com/" +
-        "indraazimi/mobpro1-compose/static-api/"
+private const val BASE_URL = "https://gh.d3ifcool.org/"
 
 private val moshi = Moshi.Builder()
     .add(KotlinJsonAdapterFactory())
@@ -20,18 +27,28 @@ private val retrofit = Retrofit.Builder()
     .build()
 
 interface HewanApiService {
-    @GET("static-api.json")
-    suspend fun getHewan(): List<Hewan>
+    @GET("hewan.php")
+    suspend fun getHewan(
+        @Header("Authorization") userId: String
+    ): List<Hewan>
+
+    @Multipart
+    @POST("hewan.php")
+    suspend fun postHewan(
+        @Header("Authorization") userId: String,
+        @Part("nama") nama: RequestBody,
+        @Part("namaLatin") namaLatin: RequestBody,
+        @Part image: MultipartBody.Part
+    ): OpStatus
 }
 
 object HewanApi {
     val service: HewanApiService by lazy {
         retrofit.create(HewanApiService::class.java)
     }
-
     fun getHewanUrl(imageId: String): String {
-        return "$BASE_URL$imageId.jpg"
+        return "${BASE_URL}image.php?id=$imageId"
     }
 }
-enum class ApiStatus { LOADING, SUCCESS,FAILED }
 
+enum class ApiStatus { LOADING, SUCCESS, FAILED }
--- app/src/main/java/org/d3if3154/mobpro1/ui/screen/MainViewModel.kt
+++ app/src/main/java/org/d3if3154/mobpro1/ui/screen/MainViewModel.kt
@@ -1,30 +1,37 @@
 package org.d3if3154.mobpro1.ui.screen
 
+import android.graphics.Bitmap
 import android.util.Log
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import org.d3if3154.mobpro1.model.Hewan
+import org.d3if3154.mobpro1.Network.ApiStatus
+import org.d3if3154.mobpro1.Network.HewanApi
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
-import org.d3if3154.mobpro1.Network.ApiStatus
-import org.d3if3154.mobpro1.Network.HewanApi
-import org.d3if3154.mobpro1.model.Hewan
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.MultipartBody
+import okhttp3.RequestBody.Companion.toRequestBody
+import java.io.ByteArrayOutputStream
 
 class MainViewModel : ViewModel() {
+
     var data = mutableStateOf(emptyList<Hewan>())
         private set
+
     var status = MutableStateFlow(ApiStatus.LOADING)
         private set
-    init {
-        retrieveData()
-    }
 
-    fun retrieveData() {
+    var errorMessage = mutableStateOf<String?>(null)
+        private set
+
+    fun retrieveData(userId: String) {
         viewModelScope.launch(Dispatchers.IO) {
             status.value = ApiStatus.LOADING
             try {
-                data.value = HewanApi.service.getHewan()
+                data.value = HewanApi.service.getHewan(userId)
                 status.value = ApiStatus.SUCCESS
             } catch (e: Exception) {
                 Log.d("MainViewModel", "Failure: ${e.message}")
@@ -32,4 +39,37 @@ class MainViewModel : ViewModel() {
             }
         }
     }
+
+    fun saveData(userId: String, nama: String, namaLatin: String, bitmap: Bitmap) {
+        viewModelScope.launch(Dispatchers.IO) {
+            try {
+                val result = HewanApi.service.postHewan(
+                    userId,
+                    nama.toRequestBody("text/plain".toMediaTypeOrNull()),
+                    namaLatin.toRequestBody("text/plain".toMediaTypeOrNull()),
+                    bitmap.toMultipartBody()
+                )
+
+                if (result.status == "success")
+                    retrieveData(userId)
+                else
+                    throw Exception(result.message)
+            } catch (e: Exception) {
+                Log.d("MainViewModel", "Failure: ${e.message}")
+                errorMessage.value = "Error: ${e.message}"
+            }
+        }
+    }
+
+    private fun Bitmap.toMultipartBody(): MultipartBody.Part {
+        val stream = ByteArrayOutputStream()
+        compress(Bitmap.CompressFormat.JPEG, 80, stream)
+        val byteArray = stream.toByteArray()
+        val requestBody = byteArray.toRequestBody(
+            "image/jpg".toMediaTypeOrNull(), 0, byteArray.size)
+        return MultipartBody.Part.createFormData(
+            "image", "image.jpg", requestBody)
+    }
+
+    fun clearMessage() { errorMessage.value = null }
 }
